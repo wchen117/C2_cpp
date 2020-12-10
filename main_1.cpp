3f852f39f9279e905639430d9740316dc2b706a1
wchen117 <wchen117@outlook.com>
2020-12-10 13:24:11 -0500

    modularized the consturction function

--- main_1.cpp
+++ main_1.cpp
@@ -1,5 +1,5 @@
 #include "main_1.hpp"
-#include "data.hpp"
+
 #include <string>
 #include <iostream>
 #include <typeinfo>
@@ -8,7 +8,7 @@
 
 int main(int args, char** argv)
 {
-    const double PI  = 3.141592653589793238463;
+    
 
     std::string data_folder = "./sample_data/ieee14/scenario_1/";
     std::string con_name = "case.con";
@@ -20,22 +20,38 @@ int main(int args, char** argv)
                   data_folder + sup_name, \
                   data_folder + con_name);
 
-    //std::string write_raw_name = "test_write.raw";
-    //std::string write_con_name = "test_write.con";
-    //std::string write_sup_name = "test_write.sup";
-    
-    //new_data.write(write_raw_name, \
-    //               write_con_name, \
-    //               write_sup_name);
-
     // case identification data from raw
     s_tilde = new_data.raw.CaseIdentificationData.sbase;
     double s_tilde_inverse = 1.0 / s_tilde;
     
-
     // bus data from raw
     int Is = new_data.raw.buses.size();
+    construct_bus(new_data);
+   
+    // load data from raw
+    construct_load(new_data, s_tilde_inverse);
     
+    // fixed shunt data from raw
+    construct_fixed_shunt(new_data, Is, s_tilde_inverse);
+   
+
+    // generator data from raw
+    int Gs = new_data.raw.generators.size();
+    construct_fixed_shunt(new_data, Gs, s_tilde_inverse);
+
+
+    
+    // line (or non-transformer branch) data from raw
+    construct_nontransformerbranch(new_data, s_tilde_inverse);
+
+    
+    
+    
+}
+
+void construct_bus(Data& new_data)
+{
+     // bus data from raw
 
     std::unordered_map<int, Bus>::iterator bus_it;
     
@@ -49,8 +65,10 @@ int main(int args, char** argv)
         v_ct_over.push_back(bus_it->second.evhi);
         v_ct_over.push_back(bus_it->second.evlo);
     }
+}
 
-    
+void construct_load(Data& new_data, double s_tilde_inverse)
+{
     // loads data from raw
     std::unordered_map<int, Load>::iterator load_it;
     for (load_it=new_data.raw.loads.begin(); load_it!=new_data.raw.loads.end(); load_it++)
@@ -70,8 +88,11 @@ int main(int args, char** argv)
     }
     // missing: Having read and processed the load section, set:
 
-    // initilize g_fs and b_fs to 0
+}
 
+void construct_fixed_shunt(Data& new_data, int Is, double s_tilde_inverse)
+{
+     // initilize g_fs and b_fs to 0
     for (int idx=0; idx < Is; idx++)
     {
         g_fs.push_back(0.0);
@@ -91,10 +112,11 @@ int main(int args, char** argv)
 
     }
 
-    // generator data from raw
-    int Gs = new_data.raw.generators.size();
-    std::unordered_map<int, Generator>::iterator gen_it;
+}
 
+void constuct_generator(Data& new_data, int Gs, double s_tilde_inverse)
+{
+    std::unordered_map<int, Generator>::iterator gen_it;
     for(gen_it=new_data.raw.generators.begin(); gen_it!=new_data.raw.generators.end(); gen_it++)
     {
         std::tuple<int, std::string> tmp_g;
@@ -102,14 +124,37 @@ int main(int args, char** argv)
         G.push_back(tmp_g);
         i_g.insert(std::make_pair(tmp_g, gen_it->second.i));
         id_g.insert(std::make_pair(tmp_g, gen_it->second.id));
-        
-       
+        p_g.insert(std::make_pair(tmp_g, gen_it->second.pg * s_tilde_inverse));
+        q_g.insert(std::make_pair(tmp_g, gen_it->second.qg * s_tilde_inverse));
+        q_g_over.insert(std::make_pair(tmp_g, gen_it->second.qt * s_tilde_inverse));
+        q_g_under.insert(std::make_pair(tmp_g, gen_it->second.qb * s_tilde_inverse));
+        p_g_over.insert(std::make_pair(tmp_g, gen_it->second.pt * s_tilde_inverse));
+        p_g_under.insert(std::make_pair(tmp_g, gen_it->second.pb * s_tilde_inverse));
+        x_g_on_0.insert(std::make_pair(tmp_g, gen_it->second.stat));
         
     }
     
+    // again, haven't implemented the having read and process the generator section, set...
+}
 
-    
+void construct_nontransformerbranch(Data& new_data, double s_tilde_inverse )
+{
+std::unordered_map<int, NontransformerBranch>::iterator non_it;
 
-    
-    
+    for(non_it= new_data.raw.nontransformerbranches.begin(); non_it!= new_data.raw.nontransformerbranches.end(); non_it++)
+    {
+        std::tuple<int, int, std::string> tmp_n;
+        double tmp_denom = std::pow(non_it->second.r, 2) + std::pow(non_it->second.x, 2);
+        tmp_n = std::make_tuple(non_it->second.i, non_it->second.j, non_it->second.ckt);
+        E.push_back(tmp_n);
+        i_e_o.insert(std::make_pair(tmp_n, non_it->second.i));
+        i_e_d.insert(std::make_pair(tmp_n, non_it->second.j));
+        id_E.insert(std::make_pair(tmp_n, non_it->second.ckt));
+        g_e.insert(std::make_pair(tmp_n, non_it->second.r/tmp_denom));
+        b_e.insert(std::make_pair(tmp_n, - non_it->second.x/tmp_denom));
+        b_e_ch.insert(std::make_pair(tmp_n, non_it->second.b));
+        r_e_over.insert(std::make_pair(tmp_n, non_it->second.ratea * s_tilde_inverse));
+        r_e_ct_over.insert(std::make_pair(tmp_n, non_it->second.ratec * s_tilde_inverse));
+        x_e_sw_0.insert(std::make_pair(tmp_n, non_it->second.st));
+    }
 }
--- main_1.hpp
+++ main_1.hpp
@@ -8,13 +8,25 @@
 #include <tuple>
 #include <string>
 #include <boost/functional/hash.hpp>
+#include "data.hpp"
+
+// some constants here
+const double PI  = 3.141592653589793238463;
 
 // some useful types
 
+// key: tuple(int, string)
 typedef std::tuple<int, std::string> key_is;
+// key: tuple(int, int, string) 
+typedef std::tuple<int, int, std::string> key_iis;
+// unordered_map using tuple(int, string) as hash key
 typedef std::unordered_map<key_is, int, boost::hash<key_is> > UMAP_TUPLE_is_INT;
 typedef std::unordered_map<key_is, double, boost::hash<key_is> > UMAP_TUPLE_is_DOUBLE;
 typedef std::unordered_map<key_is, std::string, boost::hash<key_is> > UMAP_TUPLE_is_STRING;
+// unordered map using tuple(int, int, string) as hash key
+typedef std::unordered_map<key_iis, int, boost::hash<key_iis> > UMAP_TUPLE_iis_INT;
+typedef std::unordered_map<key_iis, double, boost::hash<key_iis> > UMAP_TUPLE_iis_DOUBLE;
+typedef std::unordered_map<key_iis, std::string, boost::hash<key_iis> > UMAP_TUPLE_iis_STRING;
 
 // define the objective funcitons, assume types subject to changes
 
@@ -23,7 +35,7 @@ std::string label_k_0 = "BASECASE";
 
 std::vector<std::string> K;
 std::vector<int> A = {1, 2, 3, 4, 5, 6, 7, 8};
-std::vector<double> E;
+
 std::vector<double> F_tau;
 std::vector<double> F_theta;
 std::vector<double> F_gamma;
@@ -61,14 +73,33 @@ std::vector<double> b_fs;
 std::vector<key_is> G;
 UMAP_TUPLE_is_INT i_g;
 UMAP_TUPLE_is_STRING id_g;
-std::vector<double> p_g;
-std::vector<double> q_g;
-std::vector<double> q_t;
-std::vector<double> p_t;
-std::vector<double> p_b;
-double x_g_on_0;
+UMAP_TUPLE_is_DOUBLE p_g;
+UMAP_TUPLE_is_DOUBLE q_g;
+UMAP_TUPLE_is_DOUBLE q_g_over;
+UMAP_TUPLE_is_DOUBLE q_g_under;
+UMAP_TUPLE_is_DOUBLE p_g_over;
+UMAP_TUPLE_is_DOUBLE p_g_under;
+UMAP_TUPLE_is_DOUBLE x_g_on_0;
 
+// arrays holding line (non-transformer branch data)
+std::vector<key_iis> E;
+UMAP_TUPLE_iis_INT i_e_o;
+UMAP_TUPLE_iis_INT i_e_d;
+UMAP_TUPLE_iis_STRING id_E;
+UMAP_TUPLE_iis_DOUBLE g_e;
+UMAP_TUPLE_iis_DOUBLE b_e;
+UMAP_TUPLE_iis_DOUBLE b_e_ch;
+UMAP_TUPLE_iis_DOUBLE r_e_over;
+UMAP_TUPLE_iis_DOUBLE r_e_ct_over;
+UMAP_TUPLE_iis_DOUBLE x_e_sw_0;
 
+// functions to construct parameters using input from case.raw
+void construct_bus(Data& local_data);
+void construct_load(Data& local_data, double s_tilde_inverse);
+void construct_fixed_shunt(Data& local_data, int Is, double s_tilde_inverse);
+void construct_generator(Data& local_data, int Gs, double s_tilde_inverse);
+void construct_nontransformerbranch(Data& local_data, double s_tilde_inverse);
+void construct_transformer(Data& local_data, double s_tilde_inverse);
 
 
 
